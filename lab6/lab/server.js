69e84e7aeeaa69b0055a5b314b43ab41a30b8160
Norine NDOUDI <norinendoudi@gmail.com>
2022-11-28 13:36:36 +0100

    feat: ajout des autres lab

---
+++ lab6/lab/server.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const express = require('express');
+
+const PORT = 3000;
+
+const app = express();
+app.get('/', (req, res) => {
+  res.send('Hello World from Docker!');
+});
+
+app.listen(PORT);
+console.log(`Running on http://localhost:${PORT}`);
